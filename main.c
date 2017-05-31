52969445d305b5cf9e387759f4951a8b3f1bb7bb
Bas Bonthuis <bonthuisbas@gmail.com>
2017-05-31 11:30:11 +0200

    added empty files for IO and core

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
-#include "simulator.h"
 
-//TODO: file parser
-
-int main() {
-    printf("Hello, World!\n");
-    getc(stdin);
-    return 0;
+int main(int argc, char** argv) {
+	int isRunning = 1;
+	char readLine[100];
+	if(argc == 2)
+	{
+		//if argc == 2 the second argument is an input file
+		// calcResult(inputFile);
+		
+	}
+	 else if(argc == 3)
+	{	
+		//if argc == 3 the second argument is input, third output file
+		// calcResult(inputFile, outputFile);
+	}
+	while(isRunning)
+	{
+		fget(readLine, sizeof(readLine), stdin);
+		if(strcmp(readLine, "quit"))
+		{
+			isRunning = 0;
+		}
+	}
+	return 0;
 }
