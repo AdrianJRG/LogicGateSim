a9ce970d81335ccc6383f1bac5b28edfc23297b9
AdrianJRG <adrian.guillot@gmail.com>
2017-08-01 11:24:51 +0200

    Initial restructure and unity was added

---
+++ src/simulator.h
@@ -0,0 +1,37 @@
+//
+// Created by ajrgu on 22-May-17.
+//
+
+#ifndef LOGICGATESIM_SIMULATOR_H
+#define LOGICGATESIM_SIMULATOR_H
+
+#include <stdint.h>
+
+/* Enum used to define gate type and input */
+typedef enum {OR, AND, XOR, NOT, END, INPUT_ON, INPUT_OFF, ERROR} gate_type;
+
+/* Simulator will be stored in a binary tree */
+typedef struct GateTag{
+    //uint8_t uid;
+    char* name;
+    gate_type gate;
+    struct GateTag *left;
+    struct GateTag *right;
+} Gate;
+
+/* Function prototypes */
+int simulate_tree(Gate *gate_node);
+void destroy_tree(Gate *gate_node);
+void insert_tree(Gate *gate_nodeToInsert, Gate *gate_nodeToAttachTo);
+Gate *search_tree(uint8_t uid, Gate *gate_node);
+int count_tree(Gate *gate_node);
+
+int create_gates(int arraySize, char*** content, Gate* given_root);
+void recursive_tree_build(int arraySize, char*** content, Gate* given_gate);
+gate_type get_gate_type(char* gateString);
+int add_input_to_tree(int* input, int size_of_array, Gate* gate_node);
+void remove_inputs_from_tree(Gate* gate_node);
+void get_gate_if_exists(char* name_of_gate, int* index);
+void freeGates(Gate* given_gate);
+
+#endif //LOGICGATESIM_SIMULATOR_H
